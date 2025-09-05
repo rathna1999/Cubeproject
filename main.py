65952c5953f07d00584e614050521c551ad4fde3
root <root@ip-172-31-11-35.us-east-2.compute.internal>
2025-09-05 17:19:11 +0000

    First commit

---
+++ main.py
@@ -0,0 +1,12 @@
+# Run the server:
+# uvicorn main:app --reload
+
+from fastapi import FastAPI, Request, Form
+from fastapi.templating import Jinja2Templates
+
+app = FastAPI()
+templates = Jinja2Templates(directory="code")
+
+@app.get("/")
+def form_post(request: Request):
+    return templates.TemplateResponse("form.html", {"request": request})
